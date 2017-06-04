92d71f2c0e62abca4a430bdfe9047b8ae3031611
JiaDongXiong <842785545@qq.com>
2017-06-04 12:12:50 +0100

    Uploaded the first version of my website

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+
+  // Link modal
+  $(document).ready(function(){
+    $('.modal-trigger').leanModal();
+  });
+
+})(jQuery); // end of jQuery name space
